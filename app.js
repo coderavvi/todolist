68f5c975fe58d83fe60fbd54676793d01611e88d
CodeRavvi <iamtabson@gmail.com>
2024-05-30 11:11:32 +0100

    add gitignore

--- app.js
+++ app.js
@@ -132,7 +132,7 @@ app.post("/delete", (req, res) => {
 app.get("/about", function(req, res){
   res.render("about");
 });
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Server has started Successfully");
 });
